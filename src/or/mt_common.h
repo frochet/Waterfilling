28d07b955afb234cba6f582b1f2c1d5de0098009
Thien-Nam Dinh <thiennam.tnd@gmail.com>
2018-01-04 21:16:02 -0700

    more detailed specs on mt_common.h interface

--- src/or/mt_common.h
+++ src/or/mt_common.h
@@ -60,14 +60,14 @@ typedef struct ledger_identity_t {
 
 typedef struct ledger_t {
   ledger_identity_t identity;
-  
+
   unsigned int is_reachable:2;
   extend_info_t *ei;
-  
+
   mt_desc_t desc;
-  
+
   struct buf_t *buf;
-  
+
   uint32_t circuit_retries;
 } ledger_t;
 
@@ -176,30 +176,71 @@ int mt_process_received_directpaymentcell(circuit_t *circ, cell_t *cell);
 /************ Tor - Payment event interface *********************/
 
 /**
+ * Alert the relay controller that a payment was received by the specified client
+ *
+ * This function is invoked by the relay payment module every time that a
+ * successful payment has been made. The <b>desc</b> parameter contains the
+ * identity of the paying client. This client should be familiar to the
+ * controller through previous protocol executions.
+ */
+MOCK_DECL(int, mt_alert_payment, (mt_desc_t *desc));
+
+
+/**
  * Send a message to the given descriptor
+ *
+ * This function is invoked by a payment module when it needs to send a message
+ * to another payment module of the network. The message should be sent to the
+ * Tor instance identifed by <b>desc</b>. The possible descriptors are:
+ *   <ul>
+ *     <li> The ledger, which is loaded in via torrc config
+ *     <li> Any descriptor that has been given by the controller via <b>recv</b>
+ *     or <b>pay</b> commands
+ *   </ul>
+ * The remaining parameters correspond to the type, size, and message
+ * itself. The <b>msg</b> pointer is dynamically allocated but will be freed by
+ * the payment module upon return.
+ *
+ * The intent is that the controller will simply send the message along the
+ * network such that it is passed into <b>mt_*pay_recv</b> on the other side.
  */
 MOCK_DECL(int, mt_send_message, (mt_desc_t *desc, mt_ntype_t type, byte* msg, int size));
 
 /**
  * Send a message to the given descriptor attaching info about a 2nd descriptor
+ *
+ * This function is identical to mt_send_message except that a second descriptor
+ * is associated with the message. The controller does NOT need to sedn anything
+ * to <b>desc2</b>. Instead, it sends the message along with the value of
+ * <b>desc2</b> to <b>desc1</b>. On the other side of the network, the
+ * controller should invoke <b>mt_*pay_recv_multidesc</b>
  */
 MOCK_DECL(int, mt_send_message_multidesc, (mt_desc_t *desc1, mt_desc_t* desc2, mt_ntype_t type, byte* msg, int size));
 
 /**
- * Alert the relay controller that a payment was received by the specified client
- */
-MOCK_DECL(int, mt_alert_payment, (mt_desc_t *desc));
-
-/**
  * Inform the controller of the success of an mt_cpay_pay() protocol
+ *
+ * When the controller calls mt_cpay_pay to invoke a relay payment, the payment
+ * module will initiate whatever protocols necessary to make that happen (making
+ * some number of <b>mt_send_message</b> calls in the process). When everything
+ * is completed, it invokes <b>mt_pay_success</b> to notify the controller that
+ * the payment has gone through. The <b>rdesc</b> and <b>idesc</b> parameters
+ * are the same values that were provided in the <b>mt_cpay_pay</b> call and the
+ * <b>success</b> indicates whether the payment was completed.
+ *
+ * Note that the controller should NOT attempt to call <b>mt_cpay_pay</b> on the
+ * same <b>rdesc</b> before it has received confirmation of success.
  */
 MOCK_DECL(int, mt_pay_success, (mt_desc_t *rdesc, mt_desc_t* idesc, int success));
 
 /**
  * Inform the controller of the success of an mt_cpay_close() protocol
+ *
+ * Similar to <b>mt_pay_success</b> except to inform the controller that the
+ * <b>mt_cpay_close</b> command has completed. Once a channel has been closed,
+ * the payment module will NOT send any more message to <b>rdesc</b> again
+ * unless it is reopened.
  */
 MOCK_DECL(int, mt_close_success, (mt_desc_t *rdesc, mt_desc_t* idesc, int success));
-
-
 
 #endif
